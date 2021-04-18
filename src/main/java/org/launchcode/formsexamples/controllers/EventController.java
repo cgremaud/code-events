f91177bfac2b61cf2169e3a352efa731fd9737a7
C. Gremaud <colyn.gremaud@gmail.com>
2021-04-18 12:24:55 -0500

    added some controllers and templates. Adding fragments.

--- src/main/java/org/launchcode/formsexamples/controllers/EventController.java
+++ src/main/java/org/launchcode/formsexamples/controllers/EventController.java
@@ -3,7 +3,9 @@ package org.launchcode.formsexamples.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,14 +13,25 @@ import java.util.List;
 @Controller
 @RequestMapping("events")
 public class EventController {
+    private static List<String> events = new ArrayList<>();
+    private static String title = "Events";
+
     @GetMapping
     public String displayAllEvents(Model model){
-        List<String> events = new ArrayList<>();
-        events.add("Code With Pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
         model.addAttribute("events", events);
         return "events/index";
+    }
+
+    //lives at /events/create
+    @GetMapping("create")
+    public String renderCreateEventForm(Model model) {
+        return "events/create";
+    }
+
+    //lives at events/create but handles POST requests so it's okay
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:"; //redirects to the root path, ie events/index
     }
 }
--- src/main/java/org/launchcode/formsexamples/controllers/HomeController.java
+++ src/main/java/org/launchcode/formsexamples/controllers/HomeController.java
@@ -2,13 +2,16 @@ package org.launchcode.formsexamples.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping
-    public String index(){
+    public String index(Model model){
+        String title = "Code Events";
+        model.addAttribute("title", title);
         return "index";
     }
 
