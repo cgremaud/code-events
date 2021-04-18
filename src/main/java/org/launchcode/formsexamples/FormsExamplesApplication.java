72014dc1745a9e78e9a6372245d63be496365b43
C. Gremaud <colyn.gremaud@gmail.com>
2021-04-18 11:48:41 -0500

    initial commit

---
+++ src/main/java/org/launchcode/formsexamples/FormsExamplesApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.formsexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormsExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormsExamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/formsexamples/FormsExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.formsexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormsExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
